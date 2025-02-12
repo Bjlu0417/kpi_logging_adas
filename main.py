caed31595edab834ffa3534c0e3ee3ca2a049207
Binjun Lu <lu_binjun@hotmail.com>
2025-02-11 19:40:56 -0800

    some progress

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 
-trigger_event = []
+class Session():
+    def __init__(self, finas):
+        self.finas = "000-0000"
+    
 
 
 @app.route('/')
-def get_category():
+def show_landing_page():
     return render_template("index.html")
+
+@app.route('/new', methods = ['POST'])
+def add_new_session():
+    new_session = request.json
+    return render_template("new.html")
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
